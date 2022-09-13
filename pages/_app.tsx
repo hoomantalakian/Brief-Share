84f880e87f64b09d9404a9b265703d27f24ac59e
Hooman Talakian <hooman.talakian@gmail.com>
2022-09-14 00:54:49 +0430

    add predefined folders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/tailwind.css';
+import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
