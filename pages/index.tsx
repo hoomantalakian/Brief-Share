84114d3011de10733451c30a7a8106d9c9e9af0b
Hooman Talakian <hooman.talakian@gmail.com>
2022-09-27 23:06:09 +0330

    cleansing tailwind classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,13 +15,13 @@ export default function Home() {
           {links.map((link) => (
             <li key={link.id} className="shadow  max-w-md  rounded">
               {/* <img className="shadow-sm" src={link.imageUrl} /> */}
-              <div className="p-5 flex flex-col space-y-2">
-                <p className="text-sm text-blue-500">{link.category}</p>
-                <p className="text-lg font-medium">{link.title}</p>
-                <p className="text-gray-600">{link.description}</p>
-                <a href={link.url} className="flex hover:text-blue-500">
+              <div>
+                <p >{link.category}</p>
+                <p>{link.title}</p>
+                <p >{link.description}</p>
+                <a href={link.url} >
                   {link.url.replace(/(^\w+:|^)\/\//, '')}
-                  <svg
+                  {/* <svg
                     className="w-4 h-4 my-1"
                     fill="currentColor"
                     viewBox="0 0 20 20"
@@ -29,7 +29,7 @@ export default function Home() {
                   >
                     <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
                     <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-                  </svg>
+                  </svg> */}
                 </a>
               </div>
             </li>
