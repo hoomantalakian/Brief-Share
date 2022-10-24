c4bd96106d0c49d4b3b172941cfa508c22f74b6a
Hooman Talakian <hooman.talakian@gmail.com>
2022-10-24 21:12:34 +0330

    implementing nexus js

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,18 +1,37 @@
-import { gql } from "apollo-server-micro"
+// /graphql/schema.ts
+import { makeSchema } from 'nexus'
+import { join } from 'path'
 
-export const typeDefs = gql`
-  type Link {
-    id: String
-    title: String
-    description: String
-    url: String
-    category: String
-    imageUrl: String
-    users: [String]
-  }
+export const schema = makeSchema({
+  types: [],
+  outputs: {
+    typegen: join(process.cwd(),'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
+    schema: join(process.cwd(), 'graphql', 'schema.graphql'),
+  },
+  contextType: {
+    export: 'Context',
+    module: join(process.cwd(), 'graphql', 'context.ts'),
+  },
+})
 
-  type Query {
-    links: [Link]!
-  }
 
-`
+
+
+// import { gql } from "apollo-server-micro"
+
+// export const typeDefs = gql`
+//   type Link {
+//     id: String
+//     title: String
+//     description: String
+//     url: String
+//     category: String
+//     imageUrl: String
+//     users: [String]
+//   }
+
+//   type Query {
+//     links: [Link]!
+//   }
+
+// `
