00429767bdc96a7b8d028af90dcf1de0b5d411dd
Hooman Talakian <hooman.talakian@gmail.com>
2022-10-25 01:50:28 +0330

    nexus queries defined

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -3,25 +3,38 @@ import { objectType, extendType } from 'nexus'
 import { User } from './User'
 
 export const Link = objectType({
-  name: 'Link',
-  definition(t) {
-    t.string('id')
-    t.string('title')
-    t.string('url')
-    t.string('description')
-    t.string('imageUrl')
-    t.string('category')
-    t.list.field('users', {
-      type: User,
-      async resolve(parent, _args, ctx) {
-        return await ctx.prisma.link
-          .findUnique({
-            where: {
-              id: parent.id,
+    name: 'Link',
+    definition(t) {
+        t.string('id')
+        t.string('title')
+        t.string('url')
+        t.string('description')
+        t.string('imageUrl')
+        t.string('category')
+        t.list.field('users', {
+            type: User,
+            async resolve(parent, _args, ctx) {
+                return await ctx.prisma.link
+                    .findUnique({
+                        where: {
+                            id: parent.id,
+                        },
+                    })
+                    .users()
             },
-          })
-          .users()
-      },
-    })
-  },
+        })
+    },
+})
+
+
+export const LinksQuery = extendType({
+    type: 'Query',
+    definition(t) {
+        t.nonNull.list.field('links', {
+            type: 'Link',
+            resolve(_parent, _args, ctx) {
+                return ctx.prisma.link.findMany()
+            },
+        })
+    },
 })
